f5c93e5f40f4056926ab225fad275e04bc637449
happy-joyboy <happyk1309@gmail.com>
2024-12-05 22:57:53 +0200

    space

--- backend/logout.php
+++ backend/logout.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 // Start the session
 session_start();
@@ -11,4 +12,19 @@ session_destroy();
 // Redirect to the login page
 header("Location: login.php");
 exit();
+=======
+<?php
+// Start the session
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page
+header("Location: login.php");
+exit();
+>>>>>>> 9d9430c (Initial)
 ?>
