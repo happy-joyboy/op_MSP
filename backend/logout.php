1df8cccfdb5a6c7b5cbc85e9a800890e5e76f3e5
happy-joyboy <happyk1309@gmail.com>
2024-12-09 22:52:40 +0200

    stable fix 1

--- backend/logout.php
+++ backend/logout.php
@@ -1,7 +1,17 @@
-<<<<<<< HEAD
 <?php
 // Start the session
 session_start();
+
+// add timeout logic
+
+// if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+//     session_unset();
+//     session_destroy();
+//     header("Location: login.php?session_expired=true"); // redirect to login page
+//     exit(); // stop further script execution
+// }
+// $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
 
 // Unset all session variables
 $_SESSION = array();
@@ -10,21 +20,6 @@ $_SESSION = array();
 session_destroy();
 
 // Redirect to the login page
-header("Location: login.php");
+header("Location: ../labs/login.php");
 exit();
-=======
-<?php
-// Start the session
-session_start();
-
-// Unset all session variables
-$_SESSION = array();
-
-// Destroy the session
-session_destroy();
-
-// Redirect to the login page
-header("Location: login.php");
-exit();
->>>>>>> 9d9430c (Initial)
 ?>
